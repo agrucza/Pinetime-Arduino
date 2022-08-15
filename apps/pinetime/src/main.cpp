5a8783f0560d4603fb201f2d95760e826129a298
agrucza <a.grucza@gmx.net>
2022-08-15 18:41:14 +0200

    basic test with backlight and buzzer

--- apps/pinetime/src/main.cpp
+++ apps/pinetime/src/main.cpp
@@ -24,45 +24,29 @@
 #include "os/os.h"
 #include "bsp/bsp.h"
 #include "hal/hal_gpio.h"
-#ifdef ARCH_sim
-#include "mcu/mcu_sim.h"
-#endif
-
-static volatile int g_task1_loops;
-
-/* For LED toggling */
-int g_led_pin;
 
 /**
  * main
  *
- * The main task for the project. This function initializes packages,
- * and then blinks the BSP LED in a loop.
+ * The main task for the project.
  *
  * @return int NOTE: this function should never return!
  */
+
 int
 main(int argc, char **argv)
 {
     int rc;
 
-#ifdef ARCH_sim
-    mcu_sim_parse_args(argc, argv);
-#endif
-
     sysinit();
 
-    //g_led_pin = LED_BLINK_PIN;
-    //hal_gpio_init_out(g_led_pin, 1);
-    hal_gpio_init_out(LCD_BACKLIGHT_LOW_PIN, 0);
-    hal_gpio_init_out(LCD_BACKLIGHT_MED_PIN, 0);
-    hal_gpio_init_out(LCD_BACKLIGHT_HIGH_PIN, 0);
+    hal_gpio_init_out(LCD_BACKLIGHT_LOW_PIN, 1);
+    hal_gpio_init_out(LCD_BACKLIGHT_MED_PIN, 1);
+    hal_gpio_init_out(LCD_BACKLIGHT_HIGH_PIN, 1);
 
     hal_gpio_init_out(VIBRATOR_PIN, 1);
 
     while (1) {
-        ++g_task1_loops;
-
         /* Wait one second */
         os_time_delay(OS_TICKS_PER_SEC);
 
@@ -71,7 +55,7 @@ main(int argc, char **argv)
         hal_gpio_toggle(LCD_BACKLIGHT_MED_PIN);
         hal_gpio_toggle(LCD_BACKLIGHT_HIGH_PIN);
 
-        //hal_gpio_toggle(VIBRATOR_PIN);
+        hal_gpio_toggle(VIBRATOR_PIN);
     }
     assert(0);
 
